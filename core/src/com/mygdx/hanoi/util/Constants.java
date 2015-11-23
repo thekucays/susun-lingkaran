730e63cf2c90b1564164805dc7e1ff83ed365027
Luki R Daniel <thekucays@gmail.com>
2015-11-23 23:05:50 +0700

    [update] penambahan variabel untuk gap jika ada judul yang mau di-set koordinat nya

--- core/src/com/mygdx/hanoi/util/Constants.java
+++ core/src/com/mygdx/hanoi/util/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
 	// GUI related
 	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
 	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+	public static final float HEADER_TOP_GAP = 80.0f;
 	
 	// preferences related
 	public static final String pref_highscore = "highScores";
