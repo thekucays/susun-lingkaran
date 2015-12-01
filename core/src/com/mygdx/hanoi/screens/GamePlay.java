abca312f3631899e674b15097ae8df2928a00390
Luki R Daniel <thekucays@gmail.com>
2015-12-01 12:24:05 +0700

    [add] GamePlay starter class

---
+++ core/src/com/mygdx/hanoi/screens/GamePlay.java
@@ -0,0 +1,40 @@
+package com.mygdx.hanoi.screens;
+
+import com.badlogic.gdx.Game;
+
+public class GamePlay extends AbstractGameScreen{
+
+	public GamePlay(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void render(float deltaTime) {
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+	
+	@Override
+	public void resume() {
+
+	}
+}
