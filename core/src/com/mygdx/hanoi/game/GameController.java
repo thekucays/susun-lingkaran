8009d64f5dc178ee21c8837dba25181af6320c4d
Luki R Daniel <thekucays@gmail.com>
2015-12-02 14:34:38 +0700

    [update] spicing up controller and renderer - check ebook chp.7

--- core/src/com/mygdx/hanoi/game/GameController.java
+++ core/src/com/mygdx/hanoi/game/GameController.java
@@ -1,6 +1,12 @@
 package com.mygdx.hanoi.game;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.hanoi.game.objects.AbstractGameObject;
 import com.mygdx.hanoi.game.objects.Background;
@@ -13,22 +19,40 @@ public class GameController extends InputAdapter{
 	// game objects
 	public Array<Tiang> tiangs;
 	public Array<Ring> rings;
+	//private Game game;
 	
 	// game decorations
-	public Background background;
+	public Background background;     public Sprite[] testSprite;
+	private Game game;
 	
-	public GameController(){
+	public GameController(Game game){
+		this.game = game;
 		init();
 	}
 	
 	private void init(){
+		// preparing variables
+		rings = new Array<Ring>();
+		tiangs = new Array<Tiang>();
+		
 		initObjects();
 		initDecorations();
 		initGui();
 	}
 	
 	private void initObjects(){
+		AbstractGameObject obj = null;
+		obj = new Ring(1, "default");
+		/*obj.addListener(new ChangeListener() {
+			
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Gdx.app.log("tag", "test");
+			}
+		}); */
+		obj.position.set(0, 2);
 		
+		rings.add((Ring)obj);
 	}
 	
 	private void initDecorations(){
@@ -40,6 +64,10 @@ public class GameController extends InputAdapter{
 	}
 	
 	private void handleInput(float deltatime){
+		
+	}
+	
+	public void update(float deltaTime){
 		
 	}
 }
--- core/src/com/mygdx/hanoi/game/GameRenderer.java
+++ core/src/com/mygdx/hanoi/game/GameRenderer.java
@@ -1,9 +1,54 @@
 package com.mygdx.hanoi.game;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.mygdx.hanoi.game.objects.Ring;
+import com.mygdx.hanoi.util.Constants;
 
 public class GameRenderer implements Disposable{
 
+	private OrthographicCamera camera;
+	private GameController controller;
+	private SpriteBatch batch;
+	
+	public GameRenderer(GameController controller){
+		this.controller = controller;
+		init();
+	}
+	
+	private void init(){
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);  //diambil dari class "Constants" (di package util)
+		camera.position.set(0, 0, 0);
+		camera.update();
+	}
+	
+	public void resize(int width, int height){
+		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+		camera.update();
+	}
+	
+	public void render(){
+		renderGui();
+		renderDecorations();
+		renderObjects();
+	}
+	
+	private void renderGui(){
+		
+	}
+	
+	private void renderDecorations(){
+		
+	}
+	
+	private void renderObjects(){
+		for(Ring rings : controller.rings){
+			rings.render(batch);
+		}
+	}
+	
 	@Override
 	public void dispose() {
 		
